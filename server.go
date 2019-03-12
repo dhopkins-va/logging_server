b4c420cf17809077ccfe3e925038e03bc2c5dc08
Dallas Bobryk <dallasbobryk@gmail.com>
2019-03-11 20:00:12 -0600

    Rewrite server as an http server

--- server.go
+++ server.go
@@ -1,80 +1,73 @@
 package main
 
 import (
+	"net/http"
 	"fmt"
-	"bufio"
-	"net"
-	"strings"
+	"io/ioutil"
+	"encoding/json"
 	"os"
-	"./customLogger"
+	"time"
+	"strings"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-func init() {
+type Log struct {
 
-	// Create log folder
-	if _, err := os.Stat("./logs"); os.IsNotExist(err) {
-		os.Mkdir("./logs", os.ModePerm)
-		fmt.Println("Logs folder created")
-	}
+	Service	string `json:"service"`
+	RemoteServer string `json:"RemoteServer"`
+	Time	time.Time `json:"time"`
+	Message string `json:"message"`
 
-	logger := customLogger.GetInstance()
-	logger.Println("Init complete")
 }
-
 
 func main() {
 
-	logger := customLogger.GetInstance()
+	mux := httprouter.New()
+	mux.POST("/write", writeLogs)
+	http.ListenAndServe(":8080", mux)
+}
 
-	// Create server
-	li, err := net.Listen("tcp", ":1902")
+
+
+func writeLogs(res http.ResponseWriter, req *http.Request, params httprouter.Params) {
+
+	body, err := ioutil.ReadAll(req.Body)
+	defer req.Body.Close()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
 	}
 
-	logger.Println("Server listening...")
-
-	defer li.Close()
-	// Handle incoming connections
-	for {
-
-		conn, err := li.Accept()
-		if err != nil {
-			logger.Fatalln(err)
-		}
-
-		logger.Println("Connection received")
-		go handle(conn)
+	var log Log
+	fmt.Printf("%T", req.RemoteAddr)
+	log.RemoteServer = strings.Split(req.RemoteAddr, ":")[0]
+	err = json.Unmarshal(body, &log)
+	if err != nil {
+		fmt.Println(err)
 	}
+
+	writeToFile(log)
+
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(body)
+
 }
 
-func handle(conn net.Conn) {
+func writeToFile(log Log) {
 
-	scanner := bufio.NewScanner(conn)
-
-	for scanner.Scan() {
-		message := scanner.Text()
-		fname := strings.Split(message, " ")[0]
-		fname+= ".log"
-		writeLogs(fname, message)
-	}
-
-	defer conn.Close()
-}
-
-func writeLogs(fname string, message string) {
-
-
-	logger := customLogger.GetInstance()
-
+	fname := log.Service
 	file, err := os.OpenFile("./logs/" + fname, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		logger.Println("Error opening file for write.")
+		fmt.Println("Error opening file for write.")
 	}
 	defer file.Close()
 
-	if _, err := file.WriteString(message + "\n"); err != nil{
-		logger.Println("Error writing to file")
+	message, err := json.Marshal(log)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if _, err := file.WriteString(string(message[:]) + "\n"); err != nil{
+		fmt.Println("Error writing to file")
 	}
 
 }
